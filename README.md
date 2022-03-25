e5449c8b25fba27f35ce9ee93e532c7ea5dea901
NeYoXXX <640667912@qq.com>
2022-03-25 16:56:50 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearnTypeScript
 学习TypeScript 记录
+
+
+# 在原有的js项目中使用ts
+## 共同策略
--- vue-employee-query/types/employee-query.d.ts
+++ vue-employee-query/types/employee-query.d.ts
@@ -7,7 +7,7 @@ declare class EmployeeQuery extends Vue {
     query(): void
 }
 
-// 由于js打包为umd模块，需要增加此行
+// 由于js打包为umd模块，需要增加此行 
 export as namespace EmployeeQuery
 
 export = EmployeeQuery
