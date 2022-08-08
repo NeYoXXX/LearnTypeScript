985575b4691b1da062eb7cc7a9c0dfb81e73a2e9
尼欧 <45279363+NeYoXXX@users.noreply.github.com>
2022-08-08 16:47:41 +0800

    update

--- ts-vue/src/vue-shims.d.ts
+++ ts-vue/src/vue-shims.d.ts
@@ -1,5 +1,13 @@
+// vue2 可使用
 declare module '*.vue' {
     import Vue from 'vue'
     export default Vue
-  }
+}
+
+// vue3 使用
+// declare module '*.vue' {
+//     import type { DefineComponent } from 'vue'
+//     const component: DefineComponent<{}, {}, any>
+//     export default component
+// }
   
